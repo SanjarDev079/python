a2e83aaa9305e339da002eef812e0b23bb059812
SanjarDev079 <148218217+SanjarDev079@users.noreply.github.com>
2023-10-20 16:36:27 +0500

    Python def funksiyasi

--- def funksiyasi/def funksiyasi.py
+++ def funksiyasi/def funksiyasi.py
@@ -2,8 +2,8 @@
 
 def foydalanuvchi(a=int(input("Birinchi sonni kirting: ")),b=int(input("Ikkinchi sonni kirting: "))):
     """Sonning kvadrati va kubini hisoblovchi funksiya"""
-    print(f"Birinchi sonning kvadrati {a**2} ga teng\n"
-    f"Ikkinchi sonning kubi {b**3} ga treng.")
+    print(f"{a} ning kvadrati {a**2} ga teng\n"
+    f"{b} ning kubi {b**3} ga treng.")
 print(foydalanuvchi.__doc__)
 foydalanuvchi()
 
@@ -11,7 +11,7 @@ foydalanuvchi()
 
 def foydalanuvchi(a=int(input("Birinchi sonni kirting: ")),b=int(input("Ikkinchi sonni kirting: "))):
     """Sonning juft yoki toqligini hisoblovchi funksiya"""
-    print(f"Brinchi son {a%2==0} juft son\n"
-          f"Ikkinchi son {b%2==1} toq son")
+    print(f"{a} soni  {a%2==0} juft son\n"
+          f"{b} soni {b%2==1} toq son")
 print(foydalanuvchi.__doc__)
 foydalanuvchi()
