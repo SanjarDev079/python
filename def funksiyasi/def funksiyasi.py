9eca95a3e4953df761edf823cdfa857e6ae1d20f
SanjarDev079 <148218217+SanjarDev079@users.noreply.github.com>
2023-10-20 16:48:06 +0500

    Python def funksiyasi

--- def funksiyasi/def funksiyasi.py
+++ def funksiyasi/def funksiyasi.py
@@ -1,20 +1,20 @@
 """ UY ISHI """
 
-# def foydalanuvchi(a=int(input("Birinchi sonni kirting: ")),b=int(input("Ikkinchi sonni kirting: "))):
-#     """Sonning kvadrati va kubini hisoblovchi funksiya"""
-#     print(f"{a} ning kvadrati {a**2} ga teng\n"
-#     f"{b} ning kubi {b**3} ga treng.")
-# print(foydalanuvchi.__doc__)
-# foydalanuvchi()
-#
-#
-#
-# def foydalanuvchi(a=int(input("Birinchi sonni kirting: ")),b=int(input("Ikkinchi sonni kirting: "))):
-#     """Sonning juft yoki toqligini hisoblovchi funksiya"""
-#     print(f"{a} soni  {a%2==0} juft son\n"
-#           f"{b} soni {b%2==1} toq son")
-# print(foydalanuvchi.__doc__)
-# foydalanuvchi()
+def foydalanuvchi(a=int(input("Birinchi sonni kirting: ")),b=int(input("Ikkinchi sonni kirting: "))):
+    """Sonning kvadrati va kubini hisoblovchi funksiya"""
+    print(f"{a} ning kvadrati {a**2} ga teng\n"
+    f"{b} ning kubi {b**3} ga treng.")
+print(foydalanuvchi.__doc__)
+foydalanuvchi()
+
+
+
+def foydalanuvchi(a=int(input("Birinchi sonni kirting: ")),b=int(input("Ikkinchi sonni kirting: "))):
+    """Sonning juft yoki toqligini hisoblovchi funksiya"""
+    print(f"{a} soni  {a%2==0} juft son\n"
+          f"{b} soni {b%2==1} toq son")
+print(foydalanuvchi.__doc__)
+foydalanuvchi()
 
 
 
